180a63a12c7c0dec34de284e3b472de48cce92c1
moratorium08 <moratorium08@gmail.com>
2019-09-13 20:19:04 +0900

    Add Golang

--- spec/boxes_spec.rb
+++ spec/boxes_spec.rb
@@ -1014,4 +1014,9 @@ describe 'esolang-box', v2: true do
     it { expect(result_of(subject, 'hello.gp')).to eql("Hello, World!\n") }
     it { expect(result_of(subject, 'cat.gp', "meow")).to eql("meow\n") }
   end
+
+  describe 'golang' do
+    it { expect(result_of(subject, 'hello.go')).to eql("Hello, World!\n") }
+    it { expect(result_of(subject, 'cat.go', "meow")).to eql("meow\n") }
+  end
 end
