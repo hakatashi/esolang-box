7abcb66a9625333c303f29b2e28e9aad92270e45
Koki Takahashi <hakatasiloving@gmail.com>
2026-04-14 14:07:57 +0900

    Add Windows compatibility for Docker connection in spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
 require "docker"
 
+# On Windows, docker-api/excon does not support named pipes (npipe: scheme).
+# Use TCP instead: enable "Expose daemon on tcp://localhost:2375 without TLS"
+# in Docker Desktop Settings > General.
+if Gem.win_platform? || RUBY_PLATFORM =~ /cygwin|mswin|mingw/
+  Docker.url = ENV.fetch('DOCKER_HOST', 'tcp://127.0.0.1:2375')
+end
+
 # https://github.com/swipely/docker-api/issues/202
 Excon.defaults[:ssl_verify_peer] = false
